e1c200e9e9b264fd7e350151ca89b949634b8f4c
ClassLeeInGyu <45126633+ClassLeeInGyu@users.noreply.github.com>
2018-12-15 12:43:50 +0900

    Update Arduino_Uno.ino

--- Arduino_Uno.ino
+++ Arduino_Uno.ino
@@ -3,108 +3,121 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
+int triggerPin = 10;
+int echoPin = 9;
+int MQ135 = A0;
+int TemHumi = 7;
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 DHT dht(TemHumi, DHT11);
 
-int MQ135 = A0;
-int MQ8 = A1;
-int MQ7 = A2;
-int TemHumi = 4;
-
-
-
 void setup()
 {
- 
-pinMode(MQ135,INPUT); // 이산화탄소 검출
-pinMode(MQ8,INPUT); // 수소 문제발생
-pinMode(MQ7,INPUT); // 일산화탄소 검출 과열문제
+  pinMode(MQ135, INPUT);
+  pinMode(triggerPin,OUTPUT);
+  pinMode(echoPin,INPUT);
 
-lcd.begin();
-dht.begin();
- 
-Serial.begin(9600);
-
+  lcd.init();
+  lcd.backlight();
+  dht.begin();
+  
 }
-
-
 
 void loop()
 {
- 
-int dataMQ135 = analogRead(MQ135); // 이산화탄소
-int dataMQ8 = analogRead(MQ8); // 수소
-int dataMQ7 = analogRead(MQ7); // 일산화탄소
-float dataTemp = dht.readTemperature(); // 온도
-float dataHumi = dht.readHumidity(); // 습도
-float dataFeel = dht.computeHeatIndex(dataTemp, dataHumi, false); // 체감온도
+  int dataMQ135 = analogRead(MQ135);
+  float dataTemp = dht.readTemperature();
+  float dataHumi = dht.readHumidity();
+  float dataFeel = dht.computeHeatIndex(dataTemp, dataHumi, false);
+  digitalWrite(triggerPin, LOW);
+  digitalWrite(echoPin, LOW);
+  digitalWrite(triggerPin,HIGH);
+  delay(10);
+  digitalWrite(triggerPin,LOW);
+  int duration = pulseIn(echoPin, HIGH);
+  int distance = duration / 29 / 2;
+  
+  if( distance < 10 )
+  {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Temper : ");
+  lcd.print(dataTemp);
+  delay(1000);
+  lcd.setCursor(0,1);
+  lcd.print("State : ");
+  if (dataTemp < 24 || dataTemp > 27)
+  {
+    lcd.print("Good");
+  }
+  else if (dataTemp < 27 || dataTemp > 24)
+  {
+    lcd.print("Perfect");
+  }
+  else
+  {
+    lcd.print("Bad");
+  }
+  delay(2000);
+  lcd.clear();
 
-lcd.setCursor(0,0);
-lcd.print("Temper : ");
-lcd.print(dataTemp);
-delay(1000);
-lcd.setCursor(0,1);
-lcd.print("State : ");
-lcd.print("Perfect");
-delay(2000);
-lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Humi : ");
+  lcd.print(dataHumi);
+  lcd.setCursor(15,0);
+  lcd.print("%");
+  delay(1000);
+  lcd.setCursor(0,1);
+  lcd.print("State : ");
+  if (dataHumi < 30)
+  {
+    lcd.print("Perfect");
+  }
+  else if (dataHumi < 60)
+  {
+    lcd.print("Good");
+  }
+  else
+  {
+    lcd.print("Bad");
+  }
+  delay(2000);
+  lcd.clear();
 
-lcd.setCursor(0,0);
-lcd.print("FeelTemp : ");
-lcd.print(dataFeel);
-delay(1000);
-lcd.setCursor(0,1);
-lcd.print("State : ");
-lcd.print("Perfect");
-delay(2000);
-lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("FeelTemp : ");
+  lcd.print(dataFeel);
+  delay(1000);
+  lcd.setCursor(0,1);
+  lcd.print("Made By Lee");
+  delay(2000);
+  lcd.clear();
 
-lcd.setCursor(0,0);
-lcd.print("Humi : ");
-lcd.print(dataHumi);
-lcd.setCursor(15,0);
-lcd.print("%");
-delay(1000);
-lcd.setCursor(0,1);
-lcd.print("State : ");
-lcd.print("Perfect");
-delay(2000);
-lcd.clear();
-
-lcd.setCursor(0,0);
-lcd.print("CO2 : ");
-lcd.print(dataMQ135);
-lcd.setCursor(13,0);
-0lcd.print("ppm");
-delay(1000);
-lcd.setCursor(0,1);
-lcd.print("State : ");
-lcd.print("Perfect");
-delay(2000);
-lcd.clear();
-
-lcd.setCursor(0,0);
-lcd.print("H2 : ");
-lcd.print(dataMQ8);
-lcd.setCursor(13,0);
-lcd.print("ppm");
-delay(1000);
-lcd.setCursor(0,1);
-lcd.print("State : ");
-lcd.print("Perfect");
-delay(2000);
-lcd.clear();
-
-lcd.setCursor(0,0);
-lcd.print("CO : ");
-lcd.print(dataMQ7);
-lcd.setCursor(13,0);
-lcd.print("ppm");
-delay(1000);
-lcd.setCursor(0,1);
-lcd.print("State : ");
-lcd.print("Perfect");
-delay(2000);
-lcd.clear();
-
+  lcd.setCursor(0,0);
+  lcd.print("CO2 : ");
+  lcd.print(dataMQ135);
+  lcd.setCursor(13,0);
+  lcd.print("ppm");
+  delay(1000);
+  lcd.setCursor(0,1);
+  lcd.print("State : ");
+  if (dataHumi < 300)
+  {
+    lcd.print("Perfect");
+  }
+  else if (dataHumi < 500)
+  {
+    lcd.print("Good");
+  }
+  else
+  {
+    lcd.print("Bad");
+  }
+  delay(2000);
+  lcd.clear();
+  }
+  else
+  {
+    lcd.clear();
+  }
 }
